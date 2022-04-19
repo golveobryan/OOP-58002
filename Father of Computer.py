a96f802263f25627b9ca95a95bef1d33034e0266
golveobryan <98942853+golveobryan@users.noreply.github.com>
2022-04-19 11:18:53 +0800

    Add files via upload

---
+++ Father of Computer.py
@@ -0,0 +1,10 @@
+from tkinter import *
+win = Tk()
+win.geometry("300x200+10+20")
+win.title('Father of Computer')
+
+#1
+fthr = Label(win,text = "Charles Babbage",font='Italic', bg = "cyan")
+fthr.place(x=60, y=80)
+
+win.mainloop()
